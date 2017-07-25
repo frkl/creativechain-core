dfb2c522885451183e249befc84f636b5cfb0a5e
Andersson <ander7agar@gmail.com>
2017-07-25 14:54:01 +0200

    Make keccak hash in GetPoWHash

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -40,7 +40,11 @@ uint256 CBlockHeader::GetHash() const
 uint256 CBlockHeader::GetPoWHash() const
 {
     uint256 thash;
-    scrypt_1024_1_1_256(BEGIN(nVersion), BEGIN(thash));
+    if (nTime < KECCAK_TIME) {
+        scrypt_1024_1_1_256(BEGIN(nVersion), BEGIN(thash));
+    } else {
+        thash = SerializeKeccakHash(this);
+    }
     return thash;
 }
 
