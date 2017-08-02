65539b6f2b7a50f50d75e1e551b0f3d6e8dce696
Andersson <ander7agar@gmail.com>
2017-08-03 00:47:05 +0200

    update verification of block header

--- src/validation.cpp
+++ src/validation.cpp
@@ -3041,41 +3041,14 @@ bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& sta
     if (block.GetBlockTime() > nAdjustedTime + 2 * 60 * 60)
         return state.Invalid(false, REJECT_INVALID, "time-too-new", "block timestamp too far in the future");
 
-    // creativechain: Reject block.nVersion=1 blocks (mainnet >= 710000, testnet >= 400000, regtest uses supermajority)
-    const int nHeight = pindexPrev->nHeight+1;    
-    bool enforceV2 = false;
-    if (block.nVersion < 2) {
-        if (consensusParams.BIP34Height != -1) {
-            // Mainnet 710k, Testnet 400k
-            if (nHeight >= consensusParams.BIP34Height)
-                enforceV2 = true;
-        }
-        else {
-            // Regtest and Unittest: use Bitcoin's supermajority rule
-            if (IsSuperMajority(2, pindexPrev, consensusParams.nMajorityRejectBlockOutdated, consensusParams))
-                enforceV2 = true;
-        }
-    }
-
-    if (enforceV2) {
-        return state.Invalid(false, REJECT_OBSOLETE, strprintf("bad-version(0x%08x)", block.nVersion),
-                             strprintf("rejected nVersion=0x%08x block", block.nVersion));
-    }
-
     // Reject outdated version blocks when 95% (75% on testnet) of the network has upgraded:
-
     // check for version 2, 3 and 4 upgrades
     if((block.nVersion < 2 && nHeight >= consensusParams.BIP34Height) ||
        (block.nVersion < 3 && nHeight >= consensusParams.BIP66Height) ||
        (block.nVersion < 4 && nHeight >= consensusParams.BIP65Height))
-            return state.Invalid(false, REJECT_OBSOLETE, strprintf("bad-version(0x%08x)", block.nVersion),
-                                 strprintf("rejected nVersion=0x%08x block", block.nVersion));
-
-    if (block.nVersion < VERSIONBITS_TOP_BITS && IsWitnessEnabled(pindexPrev, consensusParams))
         return state.Invalid(false, REJECT_OBSOLETE, strprintf("bad-version(0x%08x)", block.nVersion),
-                                 strprintf("rejected nVersion=0x%08x block", block.nVersion));
+                             strprintf("rejected nVersion=0x%08x block", block.nVersion));
 
-    // Reject outdated version blocks when 75% of the network (BIP9 rules) has upgraded:
     if (block.nVersion < VERSIONBITS_TOP_BITS && IsWitnessEnabled(pindexPrev, consensusParams))
         return state.Invalid(false, REJECT_OBSOLETE, strprintf("bad-version(0x%08x)", block.nVersion),
                              strprintf("rejected nVersion=0x%08x block", block.nVersion));
