12593957cf4c0d4e4554d9e3f1f3576e0c0d6d14
Andersson <ander7agar@gmail.com>
2017-08-10 17:12:16 +0200

    deleted unused variable

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -8,9 +8,6 @@
 
 #include <stdint.h>
 
-// 2017 Aug 07 15:10:00 UTC
-static const uint32_t CHANGE_POW_TIME = 1502301600;
-
 /** The maximum allowed size for a serialized block, in bytes (only for buffer size limits) */
 static const unsigned int MAX_BLOCK_SERIALIZED_SIZE = 4000000;
 /** The maximum allowed weight for a block, see BIP 141 (network rule) */
