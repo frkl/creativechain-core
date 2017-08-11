7fc4c93f98d38d746e857f6edb5efe3fdebd04bf
Andersson <ander7agar@gmail.com>
2017-08-11 16:09:12 +0200

    fix compile error on qt tests

--- src/qt/test/test_main.cpp
+++ src/qt/test/test_main.cpp
@@ -56,11 +56,8 @@ int main(int argc, char *argv[])
     URITests test1;
     if (QTest::qExec(&test1) != 0)
         fInvalid = true;
-    CompatTests test2;
-    if (QTest::qExec(&test2) != 0)
-        fInvalid = true;
 #ifdef ENABLE_WALLET
-    PaymentServerTests test3;
+    PaymentServerTests test2;
     if (QTest::qExec(&test3) != 0)
         fInvalid = true;
 #endif
