6feacde50b3266f4d7afbc7627ae92f9fc4ab2ef
Andersson <ander7agar@gmail.com>
2017-08-11 17:16:19 +0200

    fixing warning alert

--- src/warnings.cpp
+++ src/warnings.cpp
@@ -53,8 +53,10 @@ std::string GetWarnings(const std::string& strFor)
     LOCK(cs_warnings);
 
     if (!CLIENT_VERSION_IS_RELEASE) {
-        strStatusBar = "This is a pre-release test build - use at your own risk - do not use for mining or merchant applications";
-        strGUI = _("This is a pre-release test build - use at your own risk - do not use for mining or merchant applications");
+        strStatusBar = "";
+        strGUI = _("");
+        /*strStatusBar = "This is a pre-release test build - use at your own risk - do not use for mining or merchant applications";
+        strGUI = _("This is a pre-release test build - use at your own risk - do not use for mining or merchant applications");*/
     }
 
     if (GetBoolArg("-testsafemode", DEFAULT_TESTSAFEMODE))
